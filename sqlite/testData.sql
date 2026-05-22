40ed6b0b13ffdcafd186057950aa19a43b25af12
Eric Frye <eric.frye25@gmail.com>
2026-05-22 18:17:20 -0400

    remove sqlite

--- sqlite/testData.sql
+++
@@ -1,9 +0,0 @@
-insert into LiveUpdate (EffectiveDate)
-values
-(unixepoch('2025-03-14'));
-
-insert into TournamentType (Name,CardRestriction,MaxOverall,IsQuick,IsCap,IsLive,CapAmount)
-values
-("Test","None",100,1,0,0,0),
-('League', 'None', 100, 0, 0, 0, 0),
-('Daily Rise and Shine', 'None', 100, 0,0,0,0)

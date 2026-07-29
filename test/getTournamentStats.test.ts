a012843a734c168b6b59e69ff0c7d8c850ea5d92
Eric Frye <eric.frye25@gmail.com>
2026-07-28 21:11:54 -0400

    get batting stats 2 live updates test

--- test/getTournamentStats.test.ts
+++ test/getTournamentStats.test.ts
@@ -130,3 +130,124 @@ test('Read tourney stats for 1 player', async () => {
     expect(stats['AB'] === 35).toBeTruthy();
 
 });
+
+test('Read tourney stats for 1 player when there are 2 live updates', async () => {
+
+    const datatableModelReader = new ProjectJsonModelReader<DatatableModel>("tableColumns.json");
+    const db = await initializeDatabase(databaseFilePath, datatableModelReader);
+
+    const datatableModels = await datatableModelReader.getJsonModels();
+
+    const liveUpdates: LiveUpdate[] = [
+        {
+            LiveUpdateID: 1,
+            EffectiveDate: '2026-01-01'
+        },
+        {
+            LiveUpdateID: 2,
+            EffectiveDate: '2026-06-01'
+        }
+    ]
+    const ptCards: PtCard[] = [
+        {
+            CardID: 1,
+            CardTitle: 'Bryce Harper',
+            CardType: 1,
+            LiveUpdateID: 1,
+            PtCardID: 1,
+            CardValue: 100,
+            Position: 3,
+        },
+        {
+            CardID: 1,
+            CardTitle: 'Bryce Harper',
+            CardType: 1,
+            LiveUpdateID: 2,
+            PtCardID: 2,
+            CardValue: 95,
+            Position: 3,
+        }
+    ]
+    const tournamentTypes: TournamentType[] = [
+        {
+            TournamentTypeID: 1
+        }
+    ]
+    const statsBatches: StatsBatch[] = [
+        {
+            StatsBatchID: 1,
+            TournamentTypeID: 1,
+            TournamentStartDate: '2026-01-10',
+        },
+        {
+            StatsBatchID: 2,
+            TournamentTypeID: 1,
+            TournamentStartDate: '2026-06-10',
+        },        
+        {
+            StatsBatchID: 3,
+            TournamentTypeID: 1,
+            TournamentStartDate: '2026-06-11',
+        }
+    ]
+    const battingStats: BattingStats[] = [
+        {
+            BattingStatsID: 1,
+            TeamName: 'Test',
+            PtCardID: 1,
+            StatsBatchID: 1,
+            G: 5,
+            H: 4,
+            PA: 10,
+            AB: 10,
+        },
+        {
+            BattingStatsID: 2,
+            TeamName: 'OtherTeam',
+            PtCardID: 2,
+            StatsBatchID: 2,
+            G: 2,
+            H: 3,
+            PA: 17,
+            AB: 17,
+        },        
+        {
+            BattingStatsID: 3,
+            TeamName: 'Test',
+            PtCardID: 2,
+            StatsBatchID: 3,
+            G: 5,
+            H: 6,
+            PA: 8,
+            AB: 8,
+        }
+    ]
+
+    const dataInserter = new DataInserter(db, datatableModels);
+    await dataInserter.insertRawDataAsync(liveUpdates, "LiveUpdate");    
+    await dataInserter.insertRawDataAsync(ptCards, "PtCard");
+    await dataInserter.insertRawDataAsync(tournamentTypes, "TournamentType"); 
+    await dataInserter.insertRawDataAsync(statsBatches, "StatsBatch"); 
+    await dataInserter.insertRawDataAsync(battingStats, "BattingStats"); 
+
+
+    const battingStatsFilter: BattingStatsFilter = {
+        TournamentTypeID: 1,
+        Positions: 'ANY',
+        TournamentTimeRange: {
+            StartDate: "2026-06-01",
+            EndDate: "2026-06-30",
+        }        
+    }
+    
+    const battingStatsReader: IBattingStatsGetter = new LocalSqliteBattingStatsGetter (db)
+    const tournamentBattingStats = await battingStatsReader.getTournamentStatsAsync(battingStatsFilter);
+
+    expect(tournamentBattingStats.length === 1).toBeTruthy();
+    const stats: BattingStatsExpanded = tournamentBattingStats.find(b => b.PtCardID === 2)!;
+    expect(stats['G'] === 7).toBeTruthy();
+    expect(stats['H'] === 9).toBeTruthy();
+    expect(stats['PA'] === 25).toBeTruthy();
+    expect(stats['AB'] === 25).toBeTruthy();
+
+});

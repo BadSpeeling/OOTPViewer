c79476c566af6df223acad17db8758ac9506539c
Eric Frye <eric.frye25@gmail.com>
2026-08-02 12:07:36 -0400

    remove unused utilities

--- src/utilities.ts
+++ src/utilities.ts
@@ -15,50 +15,6 @@ export function dateTime (date: Date) {
     return `${date.getFullYear()}-${digitHelper(month.toString())}-${digitHelper(dateOfMonth.toString())} ${digitHelper(hour.toString())}:${digitHelper(minute.toString())}`;
 }
 
-export function positionNumberToString (num: number) {
-    switch (num) {
-        case 1:
-            return 'P';
-        case 2:
-            return 'C';
-        case 3:
-            return '1B';
-        case 4:
-            return '2B';
-        case 5:
-            return '3B';
-        case 6:
-            return 'SS';
-        case 7:
-            return 'LF';
-        case 8:
-            return 'CF';
-        case 9:
-            return 'RF';
-    }
-}
-
-export const parseCsvDataColumnToDatatype = (type: string) => {
-    switch (type) {
-        case "DATETIME":
-            return "INTEGER";
-        default:
-            return type;
-    }
-}
-
-export const databaseObjectEqual = (databaseObject: any, testingObject: any) => {
-
-    for (const curKey of Object.keys(testingObject)) {
-        if (databaseObject[curKey] !== testingObject[curKey]) {
-            return false;
-        }
-    }
-
-    return true;
-
-}
-
 export const readFileAsync = (filePath: string[]) => {
 
     const fullFilePath = path.join(...filePath)
